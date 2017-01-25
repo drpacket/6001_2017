38fbbcee3856a5e379b6063fdd289f650a63d49b
drpacket <gottingerfrank@zoho.com>
2017-01-25 05:14:25 +0100

    Added Problem-Set 1 - Problem #3(last) - PSET01=DONE

--- ps1_work.py
+++ ps1_work.py
@@ -32,6 +32,7 @@ def vowels_in_string(s):
 print(vowels_in_string(s))
 
 
+
 # -- Problem 2 (10 Points) --
 # 10.0 points possible (graded)
 
@@ -79,68 +80,5 @@ print(count_bob(s))
 
 # - Problem Set 1 due Jan 27, 2017 00:30 CET -
 # Paste your code into this box:
-
-maxLen = 0
-sub = s[0]
-longest = s[0]
-
-for i in range(len(s) - 1):
-    if s[i + 1] >= s[i]:
-        sub += s[i + 1]
-        if len(sub) > maxLen:
-            maxLen = len(sub)
-            longest = sub
-    else:
-        sub = s[i + 1]
-        
-    i += 1
-
-print ('Longest substring in alphabetical order in string s:', longest)
-
-# --- --- --- --- --- --- --- --- --- --- --- ---
-# alpha = "abcdefghijklmnopqrstuvwxyz"
-# sub = ""
-# 
-# for i in range(len(s)):
-#     print(i, s[i])
-#     for j in range(len(alpha)):
-#         print(j, alpha[j])
-#         if s[i] == alpha[j]:
-#             sub += s[i]
-#             print(sub)
-#             break
-#     longest = sub
-#     print(longest)
-#     sub = ""
-
-
-# sub = []
-# i = 0
-# while s[i].isalpha():
-#     sub.append(s[i])
-#     i += 1
-
-
-# --- Possibly useful String-Methods ---:
-# index(...)
-#  |      S.index(sub[, start[, end]]) -> int
-#  |
-#  |      Like S.find() but raise ValueError when the substring is not found.
-# --- --- --- --- --- --- --- --- --- --- 
-# isalpha(...)
-#  |      S.isalpha() -> bool
-#  |
-#  |      Return True if all characters in S are alphabetic
-#  |      and there is at least one character in S, False otherwise.
-# --- --- --- --- --- --- --- --- --- --- 
-# rfind(...)
-#  |      S.rfind(sub[, start[, end]]) -> int
-#  |
-#  |      Return the highest index in S where substring sub is found,
-#  |      such that sub is contained within S[start:end].  Optional
-#  |      arguments start and end are interpreted as in slice notation.
-#  |
-#  |      Return -1 on failure.#
-
 
 
